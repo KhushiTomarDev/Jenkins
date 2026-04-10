991c89a91f666303e6e22bb3117a8efd28c0a01f
Khushi <khushitomar86@gmail.com>
2026-04-10 15:07:09 +0530

    commit

--- src/test/java/com/example/demo/Assignment7ApplicationTests.java
+++ src/test/java/com/example/demo/Assignment7ApplicationTests.java
@@ -3,7 +3,9 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.example.Assignment7.Application;
+
+@SpringBootTest(classes = Application.class)
 class Assignment7ApplicationTests {
 
 	@Test
